9c3237360f5acf6581fd038945a6c05cfe3132b1
0xwhitedevil <159532295+0xwhitedevil@users.noreply.github.com>
2024-03-03 13:13:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// This is a comment in JavaScript
+
+// Function to greet the user
+function greetUser(name) {
+    console.log("Hello, " + name + "! Welcome to my website.");
+}
+
+// Function to calculate the area of a rectangle
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+// flag3: _m0r3}
+function displayMessage(message) {
+    alert(message);
+}
+
+// Example usage of functions
+greetUser("John");
+var area = calculateRectangleArea(5, 3);
+console.log("Area of the rectangle: " + area);
+displayMessage("Welcome to my website!");
